27155a151b2216020e6d439645268aa23a560860
Rohit Jaiswal <114234343+rohit5038@users.noreply.github.com>
2024-08-09 21:13:46 +0530

    Add files via upload

---
+++ labs/SumOfEvenOdd.java
@@ -0,0 +1,21 @@
+package LAB;
+
+import java.util.Scanner;
+
+public class SumOfEvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Value: ");
+        int n = sc.nextInt();
+        int oddSum = 0;
+        for (int i = 1; i <= 2 * n; i += 2) {
+            oddSum += i;
+        }
+        int evenSum = 0;
+        for (int i = 2; i <= 2 * n; i += 2) {
+            evenSum += i;
+        }
+        System.out.println("Sum of first " + n + " Odd Numbers is: " + oddSum);
+        System.out.println("Sum of first " + n + " Even Numbers is: " + evenSum);
+    }
+}
